55f144a3b4c23361b0c634edab0a09e758e5b7bd
Aash <31497659+AashPau@users.noreply.github.com>
2024-04-12 14:26:36 +1000

    functionality acheived

--- src/Components/CodeGenerator.jsx
+++ src/Components/CodeGenerator.jsx
@@ -1,5 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Styles.css";
+import QRCode from "react-qr-code";
 
 export const CodeGenerator = () => {
-	return <div>CodeGenerator</div>;
+	const [qrCode, setQrCode] = useState("");
+	const [input, setInput] = useState("");
+
+	const handleGenerateQrCode = () => {
+		setQrCode(input);
+	};
+	return (
+		<div>
+			<h1>QR Code Generator</h1>
+			<div>
+				<input
+					onChange={(e) => setInput(e.target.value)}
+					type="text"
+					name="qr-code"
+					placeholder="enter your value here"
+				/>
+				<button
+					disabled={input && input.trim() !== "" ? false : true}
+					onClick={handleGenerateQrCode}
+				>
+					Generate
+				</button>
+			</div>
+			<div>
+				<QRCode id="qr-code-value" value={qrCode} size={400} bgColor="#fff" />
+			</div>
+		</div>
+	);
 };
