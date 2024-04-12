ae2e5f2f6dfa4aac30d3438ea6c49e5674c495e1
Aash <31497659+AashPau@users.noreply.github.com>
2024-04-12 14:25:08 +1000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { CodeGenerator } from "./Components/CodeGenerator";
+import "./App.css";
+
+function App() {
+	return (
+		<>
+			<div>
+				<CodeGenerator />
+			</div>
+		</>
+	);
+}
+
+export default App;
---
+++ src/Components/CodeGenerator.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const CodeGenerator = () => {
+	return <div>CodeGenerator</div>;
+};
